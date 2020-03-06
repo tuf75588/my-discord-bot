4834a4f52d3baba7a408f26f8b4118baa93b1f2c
Andrew Davis <atd285@gmail.com>
2020-03-05 21:24:16 -0500

    cache debugging

--- src/index.js
+++ src/index.js
@@ -37,4 +37,22 @@ client.on("message", async message => {
   }
 });
 
+client.on("raw", async payload => {
+  // payload will contain the type of event emitted and the user who triggered it
+
+  const { t } = payload;
+  if (t === "MESSAGE_REACTION_ADD") {
+    const { d: message_id } = payload;
+
+    // console.log(message_id); // 685310450912722987
+    console.log(client.channels.cache.get("630448799436767247"));
+  }
+});
+
+client.on("messageReactionAdd", (reaction, { username }) => {
+  // console.log(`${username} reacted with ${reaction._emoji.name}`);
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
+// 685284188546662513
